9e3be29ff53d72178afea8114c5a41e87739d69b
SwimmerPierce <chasepaddison@gmail.com>
2023-11-29 11:29:40 -0800

    commit initial changes

--- config.ts
+++ config.ts
@@ -1,8 +1,9 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://www.builder.io/c/docs/developers",
-  match: "https://www.builder.io/c/docs/**",
+  url: "https://platform.openai.com/docs/introduction",
+  match: "https://platform.openai.com/docs/**",
+  selector: '.docs-body',
   maxPagesToCrawl: 50,
   outputFileName: "output.json",
 };
