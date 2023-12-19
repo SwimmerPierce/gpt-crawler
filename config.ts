49b918033995f65bf57cbb12c81c28d32f9eb992
SwimmerPierce <chasepaddison@gmail.com>
2023-12-19 09:28:50 -0800

    changed to collect ins bond data and commented out open ai

--- config.ts
+++ config.ts
@@ -1,9 +1,19 @@
 import { Config } from "./src/config";
 
+/*
 export const defaultConfig: Config = {
   url: "https://platform.openai.com/docs/introduction",
   match: "https://platform.openai.com/docs/**",
   selector: '.docs-body',
   maxPagesToCrawl: 50,
+  outputFileName: "output.json",
+};
+*/
+
+export const defaultConfig: Config = {
+  url: "https://www.bondconnect.ca/blog/",
+  match: "https://www.bondconnect.ca/blog/**",
+  selector: '.docs-body',
+  maxPagesToCrawl: 100,
   outputFileName: "output.json",
 };
